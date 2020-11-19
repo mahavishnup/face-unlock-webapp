a2dec7238b53ac5d8f14fa57f1871650949f5142
Mahavishnu P <52984889+mahavishnup@users.noreply.github.com>
2020-11-19 12:36:23 +0530

    client and server side update

--- client/src/App.js
+++ client/src/App.js
@@ -9,9 +9,13 @@ import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import './App.css';
+const config = require("./config/keys");
 
+//You must add your own API key here from Clarifai.
 const app = new Clarifai.App({
-  apikey: "" + process.env.CLARIFAI_API_KEY + ""
+  params:{
+    apiKey: config.ClarifaiAPIKey,
+  }
 });
 
 const particlesOptions = {
---
+++ client/src/config/prod.js
@@ -0,0 +1,3 @@
+module.exports = {
+  ClarifaiAPIKey: JSON.parse(process.env.CLARIFAI_API_KEY),
+};
