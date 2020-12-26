4c2812977d2daa9d4706951f51115f33a818341c
Mahavishnu P <52984889+mahavishnup@users.noreply.github.com>
2020-12-26 13:50:43 +0530

    face unlock factor init and updating

--- client/src/components/Signin/Signin.js
+++ client/src/components/Signin/Signin.js
@@ -1,52 +1,103 @@
 import React from 'react';
+// import { Route, Router } from "react-router-dom";
+// import createHistory from "history/createBrowserHistory";
 
+// import Home from "../../views/Home";
+// import VideoInput from "../../views/VideoInput";
+
+import FaceUnlock from "../../views/VideoInput";
 class Signin extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      signInEmail: '',
-      signInPassword: ''
-    }
+      signInEmail: "",
+      signInPassword: "",
+      signInFaceUnlock: "",
+    };
   }
+
 
   onEmailChange = (event) => {
-    this.setState({signInEmail: event.target.value})
-  }
+    this.setState({ signInEmail: event.target.value });
+  };
 
   onPasswordChange = (event) => {
-    this.setState({signInPassword: event.target.value})
-  }
+    this.setState({ signInPassword: event.target.value });
+  };
+
+  onFaceUnlockChange = (event) => {
+    this.setState({ signInFaceUnlock: event.target.value });
+  };
 
   onSubmitSignIn = () => {
-    fetch('/api/signin', {
-      method: 'post',
-      headers: {'Content-Type': 'application/json'},
+    fetch("/api/signin", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
         email: this.state.signInEmail,
-        password: this.state.signInPassword
-      })
+        password: this.state.signInPassword,
+      }),
     })
-      .then(response => response.json())
-      .then(user => {
+      .then((response) => response.json())
+      .then((user) => {
         if (user.id) {
-          this.props.loadUser(user)
-          this.props.onRouteChange('home');
+          this.props.loadUser(user);
+          this.props.onRouteChange("home");
         }
-      })
-  }
+      });
+  };
+
+  onSubmitunlock = () => {
+    fetch("/api/signin/unlock", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        email: this.state.signInEmail,
+        faceunlock: this.state.signInFaceUnlock,
+      }),
+    })
+      .then((response) => response.json())
+      .then((user) => {
+        if (user.id) {
+          this.props.loadUser(user);
+          this.props.onRouteChange("home");
+        }
+      });
+  };
 
   render() {
     const { onRouteChange } = this.props;
     return (
-      <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
+      <article className="br3 ba b--white-10 mv4 w-100 w-50-m w-25-l mw6 shadow-1 center">
         <main className="pa4 black-80">
           <div className="measure">
             <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-              <legend className="f1 fw6 ph0 mh0">Sign In</legend>
+              <legend className="f1 fw6 ph0 mh0 ttu">Sign In</legend>
+
+              <div className="">
+                <FaceUnlock/>
+                {/* <input
+                  onClick={this.onSubmitunlock}
+                  className="b ph4 pv3 input-reset ba b--black bg-transparent grow pointer f6 dib"
+                  type="submit"
+                  value="Face Unlock"
+                /> */}
+                {/* <Router
+                  history={createHistory({ basename: process.env.PUBLIC_URL })}
+                >
+                  <div className="route">
+                    <Route exact path="/" component={Home} />
+                    <Route exact path="/camera" component={VideoInput} />
+                  </div>
+                </Router> */}
+              </div>
+
               <div className="mt3">
-                <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
+                <label className="db fw6 lh-copy f6" htmlFor="email-address">
+                  Email
+                </label>
                 <input
-                  className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                  className="pa2 input-reset ba bg-transparent  hover-white w-100"
                   type="email"
                   name="email-address"
                   id="email-address"
@@ -54,9 +105,11 @@ class Signin extends React.Component {
                 />
               </div>
               <div className="mv3">
-                <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
+                <label className="db fw6 lh-copy f6" htmlFor="password">
+                  Password
+                </label>
                 <input
-                  className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                  className="b pa2 input-reset ba bg-transparent hover-white w-100"
                   type="password"
                   name="password"
                   id="password"
@@ -73,7 +126,12 @@ class Signin extends React.Component {
               />
             </div>
             <div className="lh-copy mt3">
-              <p  onClick={() => onRouteChange('register')} className="f6 link dim black db pointer">Register</p>
+              <p
+                onClick={() => onRouteChange("register")}
+                className="f6 link dim black db pointer"
+              >
+                Register
+              </p>
             </div>
           </div>
         </main>
